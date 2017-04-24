5b4c1681fc197a4f4654975494de60e66afefceb
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-24 17:54:39 -0400

    if Done nano 04-condition.sh

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+note=59
 
+if test $note -lt 60
+then
+	echo "you failed !"
+elif test $note -eq 60
+then
+	echo "ouf! c'est juste!"
+else
+	echo "Alright"
+fi
 
+read -p "Entrez une lettre : " lettre
 
+case $lettre in
+	[[:lower:]])
+		echo "Lettre en minuscule"
+		;;
+	*)
+		echo "Lettre en majuscule"
+esac
 
-
-
+if test $lettre != "a"
+then
+	echo "La lettre n'est pas 'a'"
+fi
 
