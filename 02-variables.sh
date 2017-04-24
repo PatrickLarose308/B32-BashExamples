c2c3f68e177a5dbb8ef9327390fdfd614b0c999c
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-24 17:41:27 -0400

    Variables done !

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+prenom=John #Ne pas mettre d'espace !!!
+echo $prenom
+
+declare -i age=35
+declare -r age2=33 	#Devient en lecture seule (constante)
+
+cours="Cours de Linux" 	#On met des guillemets lorsqu'il y a des espaces
+echo $cours
+
+echo Le programme $0 a pris le paramètre $1
+
+
