d10c5941fe1a10596667b3849fbbf255b284f7aa
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-24 17:33:40 -0400

    Done with hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo "salut"
+#exit pour sortir du script
+echo "toi"
+
+echo -n "Vive" #-n pour écrire sur la même ligne
+echo "Linux"
+
+echo -e "1\n2\n3" #Accepter les méta-caractères (\n, \t, ...)
+
+read -p "Quel âge avez-vous ?" age #-p demande une phrase
+echo $age "ans! Vous paraissez jeune !" #$ = lorsque l'on veut la valeur de la variable (lecture
+					# sinon, on affecte dans la variable (sans le $)
