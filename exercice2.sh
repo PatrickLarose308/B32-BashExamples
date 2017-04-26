ce72f7418af33058f153ff660823309bebe88738
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-26 15:59:11 -0400

    Ex2 done

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+afficherMenu()
+{
+	choix=a
+	clear
+	while test $choix != q
+	do
+		echo "Bienvenue dans le super logiciel patente !"
+		echo "1- Dictionnaire "
+		echo "2- "
+		echo "Pour quitter, appuyez sur q"
+		read -p "Votre choix : " choix
+		if test $choix = "1"
+		then
+			./exercice1.sh
+		fi
+	done
+}
+afficherMenu
