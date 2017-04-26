a15a0422ec46df111d0cbcaada69cc4df2745870
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-26 16:35:07 -0400

    Ex3 done

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+avancer()
+{
+	for ((i=0;i<$1;i++))
+	do
+		echo -n " "
+	done
+}
+for ((i=0;i<30;i++))
+do
+	avancer $i
+	if test $i -eq 29
+	then
+		echo "BOOOOOOOOOM!!!"
+	else
+		echo -n "@"
+		sleep 0.05
+		clear
+	fi
+done
+
