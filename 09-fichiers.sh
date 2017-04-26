409e8ba9bc1f99fae063214b0d3d453041c8c6cb
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-26 15:16:13 -0400

    fichiers DONE

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+#e (si le fichier existe)	-ge = greater or equal
+if test $# -ge 1 # $# = Nombre de paramètres passés au programme 
+then
+	if test -e $1
+	then
+		echo "Il existe"
+	fi
+fi
