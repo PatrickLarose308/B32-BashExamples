831e6e3097532293858b6207caf5d352e152f446
Patrick Larose <Patrick.larose308@gmail.com>
2017-04-26 15:11:07 -0400

    Expressions régulières DONE

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# Comme read -p "Lettres: " lettre
+echo -n "Lettre: "
+read lettres
+
+# ~ = regexp
+if [[ $lettres =~ ^[a-zA-Z]+$  ]] #+ veut dire au moins 1
+then
+	echo "C'est un mot"
+fi
